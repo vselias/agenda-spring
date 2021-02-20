bbfea46fb68eb94bf9ee48c9792354df22944a22
vinicius <viniciusem@hotmail.com>
2021-02-20 09:58:24 -0300

    changes

--- src/main/java/com/example/app/security/CorsConfiguration.java
+++ src/main/java/com/example/app/security/CorsConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("https://kit.fontawesome.com/a076d05399.js").allowedMethods("GET",
+		registry.addMapping("/**").allowedOrigins("https://agenda-spring.herokuapp.com/").allowedMethods("GET",
 				"POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	}
 }
