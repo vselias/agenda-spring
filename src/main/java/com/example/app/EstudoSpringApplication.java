26b8f40a0b58db9f2d844bf2e082a7839394a95f
vinicius <viniciusem@hotmail.com>
2021-02-20 10:22:56 -0300

    xd

--- src/main/java/com/example/app/EstudoSpringApplication.java
+++ src/main/java/com/example/app/EstudoSpringApplication.java
@@ -2,6 +2,9 @@ package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EstudoSpringApplication {
@@ -10,5 +13,13 @@ public class EstudoSpringApplication {
 		SpringApplication.run(EstudoSpringApplication.class, args);
 
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
