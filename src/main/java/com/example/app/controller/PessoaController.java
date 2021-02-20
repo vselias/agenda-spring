fcbd6da1a4e3aeabeeb7aa4c54d8d7e73ce9992c
vinicius <viniciusem@hotmail.com>
2021-02-20 13:01:16 -0300

    xd

--- src/main/java/com/example/app/controller/PessoaController.java
+++ src/main/java/com/example/app/controller/PessoaController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
@@ -76,6 +77,7 @@ public class PessoaController {
 		return new Pessoa(2123223L, nome, telefone);
 	}
 
+	@Bean
 	public WebMvcConfigurer corsConfigure() {
 		return new WebMvcConfigurerAdapter() {
 			@Override
