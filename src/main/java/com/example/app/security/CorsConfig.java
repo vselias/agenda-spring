1b13ac55ddb5a77e66890db31580ed078d4be097
vinicius <viniciusem@hotmail.com>
2021-02-20 12:35:01 -0300

    xd

--- src/main/java/com/example/app/security/CorsConfig.java
+++ src/main/java/com/example/app/security/CorsConfig.java
@@ -4,20 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class CorsConfig {
 
 	@Bean
-	public WebMvcConfigurer corsConfigure() {
-		return new WebMvcConfigurer() {
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-				.allowedMethods("GET","POST","PUT","DELETE")
-				.allowedHeaders("*")
-				.allowedOrigins("*");
+				registry.addMapping("/**");
 			}
 		};
 	}
+
 }
